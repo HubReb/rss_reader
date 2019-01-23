8091d34be99db8a0e787d2f8c09353622cb6c294
Rebekka Hubert <HubReb@users.noreply.github.com>
2019-01-23 19:26:40 +0100

    add logging and add documentation

--- rss_reader.py
+++ rss_reader.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 
-""" basic rss reader """
+""" A (very) basic rss reader implementation  using a postgresql database
+
+This script accesses a changable rss feed and stores the key identifiers of an
+article (title, date of publication) in a postgresql database.
+On each call, the user is informed of new articles via a desktop notification.
+
+This tool requires an installation of postgresql on your machine as well as the
+externel python modules:
+    * psycopg2
+    * feedparser
+
+The original idea (and an implemantion of the idea using email notification and
+a sqlite database ) is accessible on
+https://fedoramagazine.org/never-miss-magazines-article-build-rss-notification-system/
+and on github:
+https://github.com/cverna/rss_feed_notifier
+"""
 
 from configparser import ConfigParser
 import subprocess
+import argparse
+import logging
 
 import psycopg2
 from psycopg2 import sql
@@ -11,6 +29,7 @@ import feedparser
 
 def read_config(filename, section):
     """ Read database configuration from file
+
     Args:
         filename (str): file containing the database specifications
         section (str): which section to take
@@ -32,12 +51,11 @@ def read_config(filename, section):
 
 def create_new_table(tablename, connection_to_db, cursor):
     """ create a new table in the database when a new feed is added """
-    data = (tablename, )
     cursor.execute(
         sql.SQL(
             "CREATE TABLE IF NOT EXISTS {} (title TEXT, date TEXT)")
         .format(sql.Identifier(tablename))
-    ) 
+    )
     connection_to_db.commit()
 
 
@@ -47,7 +65,7 @@ def insert_entry(table, title, date, connection_to_db, cursor):
     cursor.execute(
         sql.SQL(sql_instruction).format(
             sql.Identifier(table)),
-        [title,  date]
+        [title, date]
     )
     connection_to_db.commit()
 
@@ -63,7 +81,7 @@ def is_entry_in_db(title, date, table, cursor):
     if not cursor.fetchall():
         return False
     return True
- 
+
 
 def send_notification(title, url):
     """ send notification to user using notify-send """
@@ -88,10 +106,16 @@ def read_feed(feed_url, table, config_file, section):
         for article in all_articles:
             if is_entry_in_db(article["title"], article["published"], "magazine", cursor):
                 continue
-            insert_entry("magazine", article["title"], article["published"], connection_to_db,  cursor)
+            insert_entry(
+                "magazine",
+                article["title"],
+                article["published"],
+                connection_to_db,
+                cursor
+            )
             send_notification(article["title"], article["link"])
-    except (Exception, psycopg2.DataError) as error:
-        print(error)
+    except (Exception, psycopg2.DataError) as dataerror:
+        logging.exception(dataerror)
     finally:
         if connection_to_db is not None:
             connection_to_db.close()
@@ -99,7 +123,41 @@ def read_feed(feed_url, table, config_file, section):
 
 
 if __name__ == "__main__":
-    CONFIG_FiLE = "rss_db.ini"
-    SECTION = "postgresql"
-    URL = "https://fedoramagazine.org/feed/"
-    read_feed(URL, "magazine", CONFIG_FiLE, SECTION)
+    parser = argparse.ArgumentParser(
+        description='basic rss feed reader',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        default="rss_db.ini",
+        help="configuration file of the used database"
+    )
+    parser.add_argument(
+        "--section",
+        type=str,
+        default="postgresql",
+        help="section of the config file to use"
+    )
+    parser.add_argument(
+        "-u",
+        "--url",
+        type=str,
+        default="https://fedoramagazine.org/feed/",
+        help="weburl to access feed"
+    )
+    parser.add_argument(
+        "-t",
+        "--table",
+        type=str,
+        default="magazine",
+        help="table in the database to store key information of old articles in"
+    )
+    args = parser.parse_args()
+    read_feed(
+        args.url,
+        args.table,
+        args.config,
+        args.section
+    )
